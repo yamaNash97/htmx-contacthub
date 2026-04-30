4ebe2e6bc2588c023294762f21091e0ac9e196b4
Yaman Nashawati <yaman.nashawati@gmail.com>
2026-04-30 15:01:23 +0300

    video4 done

--- htmx-contacthub/contacts/views.py
+++ htmx-contacthub/contacts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
 def index(request):
-    context = {}
+    contacts = request.user.contacts.all().order_by('-date')
+    context = {'contacts': contacts }
     return render(request, 'contacts.html', context)
