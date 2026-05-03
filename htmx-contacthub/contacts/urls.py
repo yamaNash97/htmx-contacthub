804e4c0f414141be7259e01383bb416e22902ca6
Yaman Nashawati <yaman.nashawati@gmail.com>
2026-05-03 20:21:29 +0300

    video5 done hx-get hx-trigger hx-target

--- htmx-contacthub/contacts/urls.py
+++ htmx-contacthub/contacts/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('search/', views.search_contacts, name='search'),
+    
 ]
--- htmx-contacthub/contacts/views.py
+++ htmx-contacthub/contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 
 @login_required
@@ -7,3 +8,12 @@ def index(request):
     contacts = request.user.contacts.all().order_by('-date')
     context = {'contacts': contacts }
     return render(request, 'contacts.html', context)
+
+@login_required
+def search_contacts(request):
+    query = request.GET.get('search', '')
+    
+    contacts = request.user.contacts.filter(
+        Q(name__icontains= query) | Q(email__icontains=query)
+    )
+    return render(request, 'partials/contact-list.html', {'contacts': contacts} )
