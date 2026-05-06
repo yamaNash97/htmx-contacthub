2cbf4a0fdabcc8236e49725522fbc2469b289c7e
Yaman Nashawati <yaman.nashawati@gmail.com>
2026-05-06 15:30:30 +0300

    7 New Contact Form

--- htmx-contacthub/contacts/forms.py
+++ htmx-contacthub/contacts/forms.py
@@ -1,1 +1,23 @@
 from django import forms
+from .models import Contact
+
+
+class contactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields= (
+            'name', 'email'
+        )    
+        
+    name= forms.CharField(
+        widget= forms.TextInput(attrs={
+            'class': 'input input-boarder w-full',
+            'placeholder': 'Contact name'
+        })
+    )
+    email= forms.EmailField(
+         widget= forms.EmailInput(attrs={
+            'class': 'input input-boarder w-full',
+            'placeholder': 'Email Address'
+        })
+    )
--- htmx-contacthub/contacts/views.py
+++ htmx-contacthub/contacts/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from .forms import contactForm
 
 
 @login_required
 def index(request):
     contacts = request.user.contacts.all().order_by('-date')
-    context = {'contacts': contacts }
+    context = {
+        'contacts': contacts,
+        'form': contactForm()       
+               }
     return render(request, 'contacts.html', context)
 
 @login_required
